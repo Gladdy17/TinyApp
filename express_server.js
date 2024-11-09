c9678860279ebd48656258a0df9b93b7aab2798e
Gladdy17 <gladston.bosco@gmail.com>
2024-11-09 08:56:52 -0700

    added post req

--- express_server.js
+++ express_server.js
@@ -5,6 +5,17 @@ const port = 8080;
 app.set('view engine', 'ejs');
 
 
+app.use(express.urlencoded({ extended: true }));
+
+
+function generateRandomString(){
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMONPQRSTVWXYZ';
+    for (let i = 0; i < characters.length; i++){
+        result +=characters[i];
+    }
+    return result;
+}
 
 
 const urlDatabase = {
@@ -30,10 +41,22 @@ app.get('/urls', (req, res) =>{
     res.render('urls_index', templateVars);
 });
 
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
+
 app.get('/urls/:id', (req, res) => {
     const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
     res.render("urls_show", templateVars);
-})
+});
+
+
+app.post("/urls", (req, res) => {
+    const id = generateRandomString();
+    console.log(req.body); // Log the POST request body to the console
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  });
 
 app.listen(port,()=>{
     console.log(`Examples app listening on port ${port}`);
