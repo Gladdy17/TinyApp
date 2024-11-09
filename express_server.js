a48305506f545660d96c6a072263adb00defcff2
Gladdy17 <gladston.bosco@gmail.com>
2024-11-09 06:09:39 -0700

    add View files

--- express_server.js
+++ express_server.js
@@ -25,9 +25,15 @@ app.get('/urls.json', (req, res) => {
 })
 
 
+app.get('/urls', (req, res) =>{
+    const templateVars = { urls: urlDatabase };
+    res.render('urls_index', templateVars);
+});
 
-
-
+app.get('/urls/:id', (req, res) => {
+    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+    res.render("urls_show", templateVars);
+})
 
 app.listen(port,()=>{
     console.log(`Examples app listening on port ${port}`);
