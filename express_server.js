7d9a32faddbbc782abd9c20a81b01b7a98158642
Gladdy17 <gladston.bosco@gmail.com>
2024-11-12 07:58:54 -0700

    Update feature added

--- express_server.js
+++ express_server.js
@@ -65,6 +65,12 @@ app.post("/urls/:id/delete",(req, res) =>{
     delete urlDatabase[id];
     res.redirect('/urls');
 });
+app.post("/urls/:id", (req, res) =>{
+    const id = req.params.id;
+    const longURL = req.body.longURL
+    urlDatabase[id] = longURL
+    res.redirect(`/urls/${id}`)
+});
 
 app.listen(port,()=>{
     console.log(`Examples app listening on port ${port}`);
