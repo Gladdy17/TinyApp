b6e6db666dbe8704fc7048168152ad1623f6d052
Gladdy17 <gladston.bosco@gmail.com>
2024-11-12 08:32:58 -0700

    eslint added

--- express_server.js
+++ express_server.js
@@ -8,72 +8,72 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 
-function generateRandomString(){
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMONPQRSTVWXYZ';
-    for (let i = 0; i < characters.length; i++){
-        result +=characters[i];
-    }
-    return result;
+function generateRandomString() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMONPQRSTVWXYZ';
+  for (let i = 0; i < characters.length; i++) {
+    result += characters[i];
+  }
+  return result;
 }
 
 
 const urlDatabase = {
-    b2xVn2: "http://www.lighthouselabs..ca",
-    "9sm5xK": "http://www.google.com",
+  b2xVn2: "http://www.lighthouselabs..ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get('/', (req, res)=> {
-    res.send("Hello");
-})
+  res.send("Hello");
+});
 
 app.get('/hello', (req,res) =>{
-    res.send('<html><body>Hello <b>World</b></body></html>');
+  res.send('<html><body>Hello <b>World</b></body></html>');
 });
 
 app.get('/urls.json', (req, res) => {
-    res.json(urlDatabase);
-})
+  res.json(urlDatabase);
+});
 
 
 app.get('/urls', (req, res) =>{
-    const templateVars = { urls: urlDatabase };
-    res.render('urls_index', templateVars);
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+  res.render("urls_new");
 });
 
 app.get('/urls/:id', (req, res) => {
-    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
-    res.render("urls_show", templateVars);
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
 });
 
 
 app.post("/urls", (req, res) => {
-    const id = generateRandomString();
-    const longURL = req.body.longURL
-    urlDatabase[id] = longURL; // Log the POST request body to the console
-   // res.send("Ok");
-    res.redirect(`/urls/${id}`); // Respond with 'Ok' (we will replace this)
-  });
+  const id = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[id] = longURL; // Log the POST request body to the console
+  // res.send("Ok");
+  res.redirect(`/urls/${id}`); // Respond with 'Ok' (we will replace this)
+});
 
 app.post("/urls/:id/delete",(req, res) =>{
-    const id = req.params.id;
-    delete urlDatabase[id];
-    res.redirect('/urls');
+  const id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect('/urls');
 });
 app.post("/urls/:id", (req, res) =>{
-    const id = req.params.id;
-    const longURL = req.body.longURL
-    urlDatabase[id] = longURL
-    res.redirect(`/urls/${id}`)
+  const id = req.params.id;
+  const longURL = req.body.longURL;
+  urlDatabase[id] = longURL;
+  res.redirect(`/urls/${id}`);
 });
 
 app.listen(port,()=>{
-    console.log(`Examples app listening on port ${port}`);
+  console.log(`Examples app listening on port ${port}`);
 });
 
 
